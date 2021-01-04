ed974fcb8288e6808f11ee421340d34e4860068b
Alessandro Bramati <alessandro.bramati@chebanca.it>
2021-01-04 10:10:01 +0100

    unit tests

--- src/test/java/com/faire/ai/service/OpenApiWeatherServiceImplTest.java
+++ src/test/java/com/faire/ai/service/OpenApiWeatherServiceImplTest.java
@@ -1,4 +1,66 @@
 package com.faire.ai.service;
 
+import com.faire.ai.exception.ForecastNotAvailableException;
+import com.faire.ai.model.Bulletin;
+import com.faire.ai.model.openapi.OpenApiBulletin;
+import com.faire.ai.service.impl.OpenApiWeatherServiceImpl;
+import com.faire.ai.utils.BulletinUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class OpenApiWeatherServiceImplTest {
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @Mock
+    BulletinUtils utils;
+
+    @InjectMocks
+    OpenApiWeatherServiceImpl service;
+
+    @Test
+    public void testCallServiceOk() throws ForecastNotAvailableException {
+        ReflectionTestUtils.setField(service, "baseUrl", "fake/path");
+        when(restTemplate.getForEntity(anyString(), eq(OpenApiBulletin.class), any(Object.class))).thenReturn(ResponseEntity.ok().body(new OpenApiBulletin()));
+        when(utils.mapOpenApiToBulletin(any(OpenApiBulletin.class))).thenReturn(new Bulletin());
+        Bulletin bulletin = service.getForecast("Milan", Optional.empty());
+        Assertions.assertNotNull(bulletin);
+    }
+
+    @Test
+    public void testCallService401(){
+        ReflectionTestUtils.setField(service, "baseUrl", "fake/path");
+        when(restTemplate.getForEntity(anyString(), eq(OpenApiBulletin.class), any(Object.class))).thenThrow(new RestClientResponseException("Appid not valid",401,"Appid not valid",null,null,null));
+        Assertions.assertThrows(ForecastNotAvailableException.class, () -> service.getForecast("Milan", Optional.empty()));
+    }
+
+    @Test
+    public void testCallService500(){
+        ReflectionTestUtils.setField(service, "baseUrl", "fake/path");
+        when(restTemplate.getForEntity(anyString(), eq(OpenApiBulletin.class), any(Object.class))).thenThrow(RuntimeException.class);
+        Assertions.assertThrows(ForecastNotAvailableException.class, () -> service.getForecast("Milan", Optional.empty()));
+    }
+
+    @Test
+    public void testCallService404(){
+        ReflectionTestUtils.setField(service, "baseUrl", "fake/path");
+        when(restTemplate.getForEntity(anyString(), eq(OpenApiBulletin.class), any(Object.class))).thenReturn(ResponseEntity.status(HttpStatus.NOT_FOUND).body(new OpenApiBulletin()));
+        Assertions.assertThrows(ForecastNotAvailableException.class, () -> service.getForecast("Milan", Optional.empty()));
+    }
 }
--- src/test/java/com/faire/ai/utils/BulletinUtilsTest.java
+++ src/test/java/com/faire/ai/utils/BulletinUtilsTest.java
@@ -1,5 +1,80 @@
 package com.faire.ai.utils;
 
+
+import com.faire.ai.model.Bulletin;
+import com.faire.ai.model.Forecast;
+import com.faire.ai.model.openapi.OpenApiBulletin;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class BulletinUtilsTest {
 
+    BulletinUtils utils = new BulletinUtils();
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    OpenApiBulletin openApiBulletin;
+
+    Bulletin bulletin;
+
+    @BeforeEach
+    void init() throws IOException {
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        openApiBulletin = mapper.readValue(new String(Files.readAllBytes(Paths.get("src/test/resources/openapi_mock.json"))), OpenApiBulletin.class);
+        utils.setWorkingHoursInterval(Arrays.asList("09:00", "18:00"));
+        bulletin = utils.mapOpenApiToBulletin(openApiBulletin);
+    }
+
+    @Test
+    public void testMapBulletin() {
+        Assertions.assertEquals(40, bulletin.getForecast().size());
+    }
+
+    @Test
+    public void testFilterBulletin() {
+        utils.filterBulletinByCollectingForecastsForNextThreeDays(bulletin);
+        Assertions.assertEquals(24, bulletin.getForecast().size());
+    }
+
+    @Test
+    public void getMinTemp(){
+        utils.filterBulletinByCollectingForecastsForNextThreeDays(bulletin);
+        Forecast forecast = utils.getMinimumTemperature(bulletin.getForecast(), true);
+        Assertions.assertNotNull(forecast);
+        Assertions.assertEquals(1.26, forecast.getMin());
+        forecast = utils.getMinimumTemperature(bulletin.getForecast(), false);
+        Assertions.assertNotNull(forecast);
+        Assertions.assertEquals(0.15, forecast.getMin());
+    }
+
+    @Test
+    public void getMaxTemp(){
+        utils.filterBulletinByCollectingForecastsForNextThreeDays(bulletin);
+        Forecast forecast = utils.getMaximumTemperature(bulletin.getForecast(), true);
+        Assertions.assertNotNull(forecast);
+        Assertions.assertEquals(5.63, forecast.getMin());
+        forecast = utils.getMaximumTemperature(bulletin.getForecast(), false);
+        Assertions.assertNotNull(forecast);
+        Assertions.assertEquals(3.9, forecast.getMin());
+    }
+
+    @Test
+    public void getAvgHumidity(){
+        utils.filterBulletinByCollectingForecastsForNextThreeDays(bulletin);
+        Double humidity = utils.getAvgHumidity(bulletin.getForecast(), true);
+        Assertions.assertNotNull(humidity);
+        Assertions.assertEquals(70.88, humidity);
+        humidity = utils.getAvgHumidity(bulletin.getForecast(), false);
+        Assertions.assertNotNull(humidity);
+        Assertions.assertEquals(81.93, humidity);
+    }
 }
